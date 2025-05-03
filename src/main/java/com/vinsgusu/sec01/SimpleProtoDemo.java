c85150376aba6efc011a15a732373cd031881047
letiendungdt <letiendungdt@gmail.com>
2025-05-03 20:42:33 +0700

    8 create an instance from generated source

---
+++ src/main/java/com/vinsgusu/sec01/SimpleProtoDemo.java
@@ -0,0 +1,13 @@
+package com.vinsgusu.sec01;
+
+import com.vinsgusu.models.PersonOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleProtoDemo.class);
+    public static void main(String[] args) {
+        PersonOuterClass.Person person = PersonOuterClass.Person.newBuilder().setName("Vincent").setAge(20).build();
+        logger.info("Person: {}", person);
+    }
+}
